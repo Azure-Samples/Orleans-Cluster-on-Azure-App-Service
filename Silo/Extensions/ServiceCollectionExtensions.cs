dc560b0ca87f2de7e5a0fa2b89ceadb31746a369
Brady Gaster <41929050+bradygaster@users.noreply.github.com>
2022-05-03 06:17:44 -0700

    Adding AI SDK and a few other minor tweaks (#1)
    * Update deploy.yml
    
    * Update deploy.yml
    
    * getting this per the main fork
    
    * adding ai sdk
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    Revert changes to _deploy.yml_
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

---
+++ Silo/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Orleans.ShoppingCart.Silo.Extensions
+{
+    public class ApplicationMapNodeNameInitializer : ITelemetryInitializer
+    {
+        public ApplicationMapNodeNameInitializer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = Name;
+        }
+    }
+}
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationInsights(this IServiceCollection services, string applicationName)
+        {
+            services.AddApplicationInsightsTelemetry();
+            services.AddSingleton<ITelemetryInitializer>((services) => new ApplicationMapNodeNameInitializer(applicationName));
+        }
+    }
+}
--- Silo/Startup.cs
+++ Silo/Startup.cs
@@ -17,6 +17,7 @@ public sealed class Startup
         services.AddScoped<ComponentStateChangedObserver>();
         services.AddSingleton<ToastService>();
         services.AddLocalStorageServices();
+        services.AddApplicationInsights("Silo");
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
